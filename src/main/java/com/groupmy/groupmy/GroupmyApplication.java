424ec7120810654263cf8f2020f983bed5b5207d
Corso <58090876+yosbel-penate@users.noreply.github.com>
2023-01-23 18:18:40 -0500

    first commit

---
+++ src/main/java/com/groupmy/groupmy/GroupmyApplication.java
@@ -0,0 +1,13 @@
+package com.groupmy.groupmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupmyApplication.class, args);
+	}
+
+}
